39a58d90928d31b8d602fb213b8b68b9f6c29f70
trueomarson <beata4072@gmail.com>
2021-09-13 16:42:56 +0200

    initial commit

--- core/admin.py
+++ core/admin.py
@@ -8,14 +8,29 @@ from django.utils.http import urlencode
 
 from core.models import Person, Course, Grade
 
+class PersonAdminForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = "__all__"
+
+    def clean_first_name(self):
+        if self.cleaned_data["first_name"] == "Spike":
+            raise forms.ValidationError("No Vampires")
+
+        return self.cleaned_data["first_name"]
+
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "show_average")
+    search_fields = ("last_name", "first_name", )
+    fields = ("first_name", "last_name", "courses")
+    form = PersonAdminForm 
 
-    def show_average(self, obj):
-        from django.db.models import Avg 
-        from django.utlis import format_html
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["first_name"].label = "First Name (Humans Only!):"
+        return form
 
     def show_average(self, obj):
         result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
@@ -26,16 +41,17 @@ class PersonAdmin(admin.ModelAdmin):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("name", "year", "view_student_link")
+    list_display = ("name", "year", "view_students_link")
     list_filter = ("year", )
 
-    def view_student_link(self, obj):
+    def view_students_link(self, obj):
         count = obj.person_set.count()
         url = (
             reverse("admin:core_person_changelist")
             + "?"
-            + urlencode({"course_id": f"{obj.id}"})
+            + urlencode({"courses__id": f"{obj.id}"})
         )
+
         return format_html('<a href="{}">{} Students</a>', url, count)
 
 @admin.register(Grade)
